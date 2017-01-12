d00d3126f0754a8fd01f011b84e973adb92a4cd3
Lukasz Ziobron <lukin1000@gmail.com>
2017-01-12 12:42:53 +0100

    Fill forward and fill backward algorithm used. UT passed.

--- board.py
+++ board.py
@@ -20,18 +20,46 @@ class Board(object):
                 print(item, end='')
             print('')
 
+    def __fill_row_forward(self, row):
+        internal_row = [self.UNKNOWN] * self.size_h
+        index = 0
+        for value, number in enumerate(row):
+            for x in range(number):
+                internal_row[index + x] = value
+                if index + x + 1 < self.size_h:
+                    internal_row[index + x + 1] = self.WHITE
+            index += number + 1
+        return internal_row
+
+    def __fill_row_backward(self, row):
+        internal_row = [self.UNKNOWN] * self.size_h
+        max_val = len(row) - 1
+        index = self.size_h - 1
+        for value, number in enumerate(reversed(row)):
+            for x in range(number):
+                internal_row[index - x] = max_val - value
+                if index - x - 1 > 0:
+                    internal_row[index - x - 1] = self.WHITE
+            index -= x + 2
+        return internal_row
+
     def mark_horizontal_intersections(self):
         for i, row in enumerate(self.horizontal_data):
+            if not row:
+                raise LookupError("Row cannot be empty")
             row_size = sum(row) + len(row) - 1
             if row_size == self.size_h:
                 self.board[i] = self.full_row_match(row)
             else:
-                size_diff = self.size_h - row_size
-                for number in row:
-                    if number > size_diff:
-                        filled_size = number - size_diff
-                        for f in range(filled_size):
-                            self.board[i][size_diff + f] = self.BLACK
+                row_forward = self.__fill_row_forward(row)
+                row_backward = self.__fill_row_backward(row)
+                print(row_forward)
+                print(row_backward)
+                for index in range(self.size_h):
+                    if row_forward[index] == self.UNKNOWN or row_forward[index] == self.WHITE:
+                        continue
+                    if row_forward[index] == row_backward[index]:
+                        self.board[i][index] = self.BLACK
 
     def full_row_match(self, row):
         output_row = []
