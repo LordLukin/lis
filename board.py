b77ec005816e32545aba759029a2213d1e9f9b10
Lukasz Ziobron <lukin1000@gmail.com>
2017-01-13 11:45:40 +0100

    Marking unreachable fields added

--- board.py
+++ board.py
@@ -34,14 +34,14 @@ class Board(object):
     def solve(self):
         self.mark_intersections(Direction.HORIZONTAL)
         self.mark_intersections(Direction.VERTICAL)
-        self.mark_unreachable_fields_horizontally()
-        self.mark_unreachable_fields_vertically()
-        #self.extend_border_markers()
+        # self.extend_border_markers()
+        self.mark_unreachable_fields(Direction.HORIZONTAL)
+        self.mark_unreachable_fields(Direction.VERTICAL)
 
     def print_board(self):
-        unknown = u"\u2591" #* 2
-        black = u"\u2588" #* 2
-        white = u" " #* 2
+        unknown = u"\u2591"  # * 2
+        black = u"\u2588" * 2
+        white = u" "  # * 2
 
         print('+', end='')
         print('-' * self.size_h, end='')
@@ -49,7 +49,7 @@ class Board(object):
         for row in self.board:
             print('|', end='')
             for item in row:
-                #print(item, end='') # debug print
+                print(item, end='') # debug print
                 if item == self.WHITE:
                     print(white, end='')
                 elif item == self.UNKNOWN:
@@ -85,12 +85,13 @@ class Board(object):
         return internal_row
 
     def mark_intersections(self, direction):
-        if direction == Direction.HORIZONTAL:
-            data = self.horizontal_data
-            size = self.size_h
-        else:
+        if direction == Direction.VERTICAL:
             data = self.vertical_data
             size = self.size_v
+            self.board = np.transpose(self.board)
+        else:
+            data = self.horizontal_data
+            size = self.size_h
 
         for i, row in enumerate(data):
             row_size = sum(row) + len(row) - 1
@@ -100,15 +101,9 @@ class Board(object):
                 row_backward = self.__fill_backward(row)
                 for index in range(size):
                     if row_forward[index] == self.WHITE:
-                        if direction == Direction.HORIZONTAL:
-                            self.board[i][index] = self.WHITE
-                        else:
-                            self.board[index][i] = self.WHITE
+                        self.board[i][index] = self.WHITE
                     elif row_forward[index] == row_backward[index]:
-                        if direction == Direction.HORIZONTAL:
-                            self.board[i][index] = self.BLACK
-                        else:
-                            self.board[index][i] = self.BLACK
+                        self.board[i][index] = self.BLACK
             else:
                 row_forward = self.__fill_forward(row)
                 row_backward = self.__fill_backward(row)
@@ -116,53 +111,40 @@ class Board(object):
                     if row_forward[index] == self.UNKNOWN or row_forward[index] == self.WHITE:
                         continue
                     if row_forward[index] == row_backward[index]:
-                        if direction == Direction.HORIZONTAL:
-                            self.board[i][index] = self.BLACK
-                        else:
-                            self.board[index][i] = self.BLACK
+                        self.board[i][index] = self.BLACK
 
-    def mark_unreachable_fields_horizontally(self):
-        pass
-        # for i, row in enumerate(self.horizontal_data):
-        #     if self.BLACK in self.board[i]:
-        #         if len(row) == 1:
-        #             filled = self.board[i].count(self.BLACK)
-        #             diff = row[0] - filled
-        #             for index in reversed(range(self.size_h)):
-        #                 if self.board[i][index] == self.BLACK:
-        #                     break
-        #             first = index + diff + 1
-        #             if first < self.size_h:
-        #                 for j in range(first, self.size_h):
-        #                     self.board[i][j] = self.WHITE
-        #             print(i, index, index + diff + 1)
-        #             for index in range(self.size_h):
-        #                 if self.board[i][index] == self.BLACK:
-        #                     break
-        #             last = index - diff
-        #             if last > 0:
-        #                 for j in range(last):
-        #                     self.board[i][j] = self.WHITE
+        if direction == Direction.VERTICAL:
+            self.board = np.transpose(self.board)
 
-    def mark_unreachable_fields_vertically(self):
-        pass
-        # for i, row in enumerate(self.vertical_data):
-        #     if self.BLACK in self.board[:][i]:
-        #         if len(row) == 1:
-        #             filled = self.board[:][i].count(self.BLACK)
-        #             diff = row[0] - filled
-        #             for index in reversed(range(self.size_v)):
-        #                 if self.board[i][index] == self.BLACK:
-        #                     break
-        #             first = index + diff + 1
-        #             if first < self.size_h:
-        #                 for j in range(first, self.size_v):
-        #                     self.board[i][j] = self.WHITE
-        #             print(i, index, index + diff + 1)
-        #             for index in range(self.size_v):
-        #                 if self.board[i][index] == self.BLACK:
-        #                     break
-        #             last = index - diff
-        #             if last > 0:
-        #                 for j in range(last):
-        #                     self.board[i][j] = self.WHITE
+    def mark_unreachable_fields(self, direction):
+        if direction == Direction.VERTICAL:
+            data = self.vertical_data
+            size = self.size_v
+            self.board = np.transpose(self.board)
+        else:
+            data = self.horizontal_data
+            size = self.size_h
+
+        for i, row in enumerate(data):
+            if self.BLACK in self.board[i]:
+                if len(row) == 1:
+                    filled = np.count_nonzero(self.board[i] == self.BLACK)
+                    diff = row[0] - filled
+                    for index in reversed(range(size)):
+                        if self.board[i][index] == self.BLACK:
+                            break
+                    first = index + diff + 1
+                    if first < self.size_h:
+                        for j in range(first, size):
+                            self.board[i][j] = self.WHITE
+                    print(i, index, index + diff + 1)
+                    for index in range(size):
+                        if self.board[i][index] == self.BLACK:
+                            break
+                    last = index - diff
+                    if last > 0:
+                        for j in range(last):
+                            self.board[i][j] = self.WHITE
+
+        if direction == Direction.VERTICAL:
+            self.board = np.transpose(self.board)
